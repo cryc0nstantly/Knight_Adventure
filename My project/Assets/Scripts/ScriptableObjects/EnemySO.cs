edcf176f8ac2aece917d5f91bfb641b004d0090f
cryc0nstantly <tihonova.aksinya@gmail.com>
2025-12-17 02:55:30 +0400

    My game Knight Adventure

---
+++ My project/Assets/Scripts/ScriptableObjects/EnemySO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+
+public class EnemySO : ScriptableObject
+{
+    public string enemyName;
+    public int enemyHealth;
+    public int enemyDamageAmount;
+}
